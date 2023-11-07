3cff20ad6c1652a31203ef9566c1d818c4d1ed5e
varun vatsal <varunvatsal963@gmail.com>
2023-11-07 16:47:27 +0530

    learnt to use enviorment variables

--- project01/index.js
+++ project01/index.js
@@ -1,11 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+// const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/varun', (req, res) => {
+  res.send('@varunvatsal3')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1> login on twitter </h1>')
+})
+
+app.get('/youtube', (req, res) => {
+  res.redirect('https://www.youtube.com/watch?v=pOV4EjUtl70')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
