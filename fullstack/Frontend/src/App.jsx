ad9dcd1c3f96fe464fd59086c9e6561975f65a14
varun vatsal <varunvatsal963@gmail.com>
2023-11-17 05:36:15 +0530

    added proxy for /api

--- fullstack/Frontend/src/App.jsx
+++ fullstack/Frontend/src/App.jsx
@@ -5,9 +5,10 @@ function App() {
   const [jokes, setJokes] = useState([]);
 
   useEffect(()=>{
-    axios.get('http://localhost:4000/api/jokes')
+    axios.get('/api/jokes')
     .then((res)=>{
       setJokes(res.data)
+      console.log(res.data)
     }).catch((err)=>{
       console.log(error)
     })
@@ -18,12 +19,12 @@ function App() {
       <h1>hello brother</h1>
       <p>JOKES: {jokes.length}</p>
       {
-        jokes.map((joke) => {
+        jokes.map((joke) => (
           <div key={joke.id}>
-            <h3>{joke.title}</h3>
-            <p>{joke.content}</p>
+            <h3>{joke.title} : {joke.content}</h3>
+            
           </div>
-        } )
+         ) )
       }
     </>
   )
