df78fd7e934811facc1a995186f947291bd37973
varun vatsal <varunvatsal963@gmail.com>
2024-01-18 22:24:03 +0530

    basic changes

--- youtubeClone/src/controllers/user.controller.js
+++ youtubeClone/src/controllers/user.controller.js
@@ -223,7 +223,7 @@ const changeCurrentPassword = asyncHandler(async(req, res)=>{
 const getCurrentUser = asyncHandler(async(req, res)=>{
     return res
     .status(200)
-    .json(200, req.user, "currenct user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res)=>{
@@ -233,7 +233,7 @@ const updateAccountDetails = asyncHandler(async(req, res)=>{
         throw new ApiError(400, "all fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -250,6 +250,7 @@ const updateAccountDetails = asyncHandler(async(req, res)=>{
     
 })
 
+// create a function to delete old avatar or cover image
 const updateUserAvatar = asyncHandler(async (req, res)=>{
     const avatarLocalPath = req.file?.path
 
@@ -305,6 +306,8 @@ const updateUserCoverImage = asyncHandler(async (req, res)=>{
     .status(200)
     .json(new ApiResponse(200, user, "cover image updated succefully"))
 })
+
+
 
 
 
