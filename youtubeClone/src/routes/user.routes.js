42c7f07b0b1a6a88e927252d651f4f37670ec19d
varun vatsal <varunvatsal963@gmail.com>
2024-01-22 03:49:18 +0530

    added routes

--- youtubeClone/src/routes/user.routes.js
+++ youtubeClone/src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js"
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar,
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js"
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 const userRouter = Router()
@@ -22,4 +34,13 @@ userRouter.route("/login").post(loginUser)
 //secured routes
 userRouter.route("/logout").post(verifyJWT, logoutUser)
 userRouter.route("/refreshAccessToken").post(refreshAccessToken)//end point to renew access token
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails)
+userRouter.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+userRouter.route("/update-coverImage").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+userRouter.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+//:username name should be same as written in the controller
+userRouter.route("/history").get(verifyJWT, getWatchHistory)
+
 export default userRouter 
